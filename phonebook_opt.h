6f3d81b1add1a868586324fd87920a2e6d0f0ff2
William Chang <william80121@hotmail.com.tw>
2017-10-16 22:19:59 +0800

    Change the data structure use linked list

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,14 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+#define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_DET *pDet;
+    struct __PHONE_BOOK_ENTRY *pNext;
+} entry;
+
+typedef struct __PHONE_BOOK_DET {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,8 +22,7 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
-} entry;
+} det;
 
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
